4e0fa4a99fe043b31977c9cc13df0bff8cbeb493
Sean McFeely <mcfeelynaes@gmail.com>
2021-04-14 13:42:39 -0400

    Up version

--- tests/test_cbinterface.py
+++ tests/test_cbinterface.py
@@ -7,7 +7,7 @@ HOME_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def test_version():
     from cbinterface import __version__
-    assert __version__ == "2.3.6"
+    assert __version__ == "2.3.7"
 
 def test_timezone_settings():
     from dateutil import tz
